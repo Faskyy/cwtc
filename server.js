14fbe9a07873ed4ffcff35a190ca62a05a6ffb1c
fahdk <fahdk@temple.edu>
2022-01-14 12:19:55 -0500

    emptied dist

---
+++ server.js
@@ -0,0 +1,18 @@
+/*const express = require('express')
+const path = require('path')ß
+const app = express()
+
+const PORT = process.env.PORT|| 5500
+
+app.use(express.static('dist'));
+
+app.get('/', ()=>{
+    
+})
+
+
+app.listen(PORT, ()=>{
+    console.log
+})
+
+*/
